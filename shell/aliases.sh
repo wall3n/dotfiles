575f7c192192e554ef771a32123155b2c98e31be
wall3n <franmoreno1506@gmail.com>
2022-05-08 22:19:14 +0200

    Adding new aliases for editors and ls command

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,6 +3,7 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
+alias ls="lsd"
 alias ll="ls -l"
 alias la="ls -la"
 alias ~="cd ~"
@@ -31,6 +32,9 @@ alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
+
+# Editors
+alias idea='~/JetBrains/idea'
 
  # Custom scripts
  alias key='~/.dotfiles/bin/key.sh'
