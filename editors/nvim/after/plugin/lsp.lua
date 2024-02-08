d89cbd3110344de9accecb326ebdf8b149db029b
Wall3n <franmoreno1506@gmail.com>
2024-02-08 21:28:19 +0100

    Adding configuration and remaps for LSP-Zero

---
+++ editors/nvim/after/plugin/lsp.lua
@@ -0,0 +1,31 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+	'tsserver',
+	'eslint',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action() 
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		['<C-Space>'] = cmp.mapping.complete(),
+	})
+})
+
+
